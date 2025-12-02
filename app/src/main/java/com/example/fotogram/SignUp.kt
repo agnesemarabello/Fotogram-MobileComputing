c55a7c97034de107432e8db5d3b049ae48e5bb83
Agnese Marabello <agnesemarabello@MacBook-Air-di-Agnese.local>
2025-12-02 11:54:09 +0100

    SignUp -> Miglioramento grafica

--- app/src/main/java/com/example/fotogram/SignUp.kt
+++ app/src/main/java/com/example/fotogram/SignUp.kt
@@ -1,20 +1,25 @@
 package com.example.fotogram
 
 import android.net.Uri
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
@@ -25,84 +30,130 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.runtime.*
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.White
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.ui.draw.clip
 
 @Composable
 fun SignUp() {
     var username by remember { mutableStateOf("") }
     var profileImageUri by remember { mutableStateOf<Uri?>(null) }
 
-    Column (
+    Box(
         modifier = Modifier
             .fillMaxSize()
-            .statusBarsPadding(),
-
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+            .background(Color(0xFF90CAF9)),
+        contentAlignment = Alignment.Center
     ) {
-        Text(
-            text = "Benvenuto in Fotogram!",
-            style = MaterialTheme.typography.headlineMedium
-        )
-
-
-        Spacer(modifier = Modifier.height(150.dp))
-
-        Row (
+        Column(
             modifier = Modifier
-                .fillMaxWidth(),
-            verticalAlignment = Alignment.CenterVertically,
+                .fillMaxWidth(0.9f)
+                .fillMaxHeight(0.4f)
+                .background(
+                    Color(0xFFF5F5F5),
+                    shape = RoundedCornerShape(16.dp)
+                )
+                .padding(20.dp),
+
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
         ) {
-            OutlinedTextField(
-                value = username,
-                onValueChange = { newValue ->
-                    username = newValue
-                },
-
-                supportingText = {
-                    if (username.length > 15) {
-                        Text(
-                            "max 15 caratteri",
-                            color = MaterialTheme.colorScheme.error
-                        )
-                    }
-                },
-
-                label = { Text("Inserisci il tuo username") },
-                singleLine = true,
-                modifier = Modifier
-                    .padding(horizontal = 20.dp)
-                    .weight(1f)
-                    .padding(start = 20.dp)
+            Text(
+                text = "Benvenuto in Fotogram!",
+                style = MaterialTheme.typography.headlineMedium
             )
 
-            Button(
-                onClick = {},
-                shape = CircleShape,
-                modifier = Modifier
-                    .padding(end =  10.dp)
-                    .size(68.dp)
-                ) {
 
-                if(profileImageUri == null) {
-                    Icon(
-                        imageVector = Icons.Filled.Person,
-                        contentDescription = "Seleziona immagine profilo",
-                        tint = MaterialTheme.colorScheme.onPrimary,
-                        modifier = Modifier.fillMaxSize()
-                    )
+            Spacer(modifier = Modifier.height(60.dp))
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 5.dp),
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                OutlinedTextField(
+                    value = username,
+                    onValueChange = { newValue ->
+                        username = newValue
+                    },
+
+                    supportingText = {
+                        if (username.length > 15) {
+                            Text(
+                                "max 15 caratteri",
+                                color = MaterialTheme.colorScheme.error
+                            )
+                        }
+                    },
+
+                    label = { Text("Inserisci il tuo username") },
+                    singleLine = true,
+                    modifier = Modifier
+                        .padding(horizontal = 10.dp)
+                        .weight(1f)
+                )
+
+                Box(
+                    modifier = Modifier
+                        .size(80.dp)
+                        .align(Alignment.CenterVertically),
+                    contentAlignment = Alignment.TopStart
+                ) {
+                    Button(
+                        onClick = {},
+                        shape = CircleShape,
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = MaterialTheme.colorScheme.primary,
+                            contentColor = White
+                        ),
+                        modifier = Modifier
+                            .size(70.dp)
+                            .align(Alignment.TopStart)
+                    ) {
+                        Box(
+                            modifier = Modifier.fillMaxSize(),
+                            contentAlignment = Alignment.Center
+                        ) {
+
+                            Icon(
+                                imageVector = Icons.Filled.Person,
+                                contentDescription = "Seleziona immagine profilo",
+                                modifier = Modifier.fillMaxSize()
+                            )
+                            Box(
+                                modifier = Modifier
+                                    .align(Alignment.BottomEnd)
+                                    .offset(x = 20.dp, y = 10.dp)
+                                    .size(24.dp)
+                                    .clip(CircleShape)
+                                    .background(Color(0xFF90CAF9)),
+                                contentAlignment = Alignment.Center
+                            ) {
+                                Icon(
+                                    imageVector = if (profileImageUri == null) Icons.Filled.Add else Icons.Filled.Check,
+                                    contentDescription = "Stato selezione immagine",
+                                    modifier = Modifier.size(20.dp)
+                                )
+                            }
+                        }
+                    }
                 }
             }
 
-        }
+            Spacer(modifier = Modifier.height(16.dp))
 
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Button(onClick = {}) {
-            Text(text = "Registrati")
+            Button(
+                onClick = {},
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 35.dp)
+            ) {
+                Text(text = "Registrati e inizia")
+            }
         }
     }
 }
